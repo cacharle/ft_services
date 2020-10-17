b1b303e0cb3f5e0943736d0807debf76c4242693
Charles Cabergs <me@cacharle.xyz>
2020-10-17 14:44:14 +0200

    Added mysql volume (not working), Added grafana and influxdb Dockerfile

--- setup.sh
+++ setup.sh
@@ -1,22 +1,34 @@
 #!/bin/sh
 
-names='
-ftps
-'
+# names='
+# ftps
+# '
 
 # nginx
 # wordpress
 # phpmyadmin
 
+
 echo 'Building docker images'
-echo "$names" | xargs -Iname docker build -t cacharle-name srcs/name
+docker build -t cacharle-ftps       srcs/ftps
+docker build -t cacharle-nginx      srcs/nginx
+docker build -t cacharle-wordpress  srcs/wordpress
+docker build -t cacharle-phpmyadmin srcs/phpmyadmin
+docker build -t cacharle-mysql      srcs/mysql
 
-minikube start
-
-eval $(minikube docker-env)
-
-echo 'Creating cluster'
-echo "$names" | xargs -Iname kubectl apply -f srcs/name/name.yaml
+# minikube start
+#
+# eval $(minikube docker-env)
 
 
-minikube dashboard
+kubectl apply -f srcs/ftps/ftps.yaml
+kubectl apply -f srcs/nginx/nginx.yaml
+kubectl apply -f srcs/wordpress/wordpress.yaml
+kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f srcs/mysql/mysql.yaml
+
+# echo 'Creating cluster'
+# echo "$names" | xargs -Iname kubectl apply -f srcs/name/name.yaml
+
+#
+# minikube dashboard
