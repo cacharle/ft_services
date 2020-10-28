7de2bedaae3bd64894a497e31a44914d4c033531
Charles Cabergs <me@cacharle.xyz>
2020-10-28 15:13:08 +0100

    Added influx and grafana to kubernetes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# names='
-# ftps
-# '
-
-# nginx
-# wordpress
-# phpmyadmin
-
 
 echo 'Building docker images'
 docker build -t cacharle-ftps       srcs/ftps
@@ -15,6 +7,8 @@ docker build -t cacharle-nginx      srcs/nginx
 docker build -t cacharle-wordpress  srcs/wordpress
 docker build -t cacharle-phpmyadmin srcs/phpmyadmin
 docker build -t cacharle-mysql      srcs/mysql
+docker build -t cacharle-grafana    srcs/grafana
+docker build -t cacharle-influxdb   srcs/influxdb
 
 # minikube start
 #
@@ -26,6 +20,8 @@ kubectl apply -f srcs/nginx/nginx.yaml
 kubectl apply -f srcs/wordpress/wordpress.yaml
 kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f srcs/mysql/mysql.yaml
+kubectl apply -f srcs/grafana/grafana.yaml
+kubectl apply -f srcs/influxdb/influxdb.yaml
 
 # echo 'Creating cluster'
 # echo "$names" | xargs -Iname kubectl apply -f srcs/name/name.yaml
