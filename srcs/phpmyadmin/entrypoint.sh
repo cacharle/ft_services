af1504d72a3be36a26a27632184320abd64d1297
Charles Cabergs <me@cacharle.xyz>
2020-10-28 11:22:02 +0100

    Added wordpress and phpmyadmin with nginx, Added nginx ssh liveliness probe

---
+++ srcs/phpmyadmin/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+nginx -t || exit 1
+
+openrc
+touch /run/openrc/softlevel
+
+rc-update add php-fpm7 default
+rc-update add nginx default
+rc-service nginx restart
+rc-service php-fpm7 restart
+
+sleep infinity &
+wait
